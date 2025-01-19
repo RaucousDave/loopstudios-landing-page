14fd7a5ee0ca0dac85c841bcf1b1ab6bf417e45b
Davies Enienghan <daviesenienghan906@gmail.com>
2025-01-19 10:08:28 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('.hamburger-menu');
+const  menu = document.querySelector('.mobile-menu');
+
+function showMenu() {
+    menu.style.display = 'flex';
+}
+
+function hideMenu() {
+    menu.style.display = 'none';
+}
